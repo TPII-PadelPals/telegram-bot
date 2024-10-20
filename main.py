a856c2d051ddc686d589f28ce2d1c7a9355f2bb3
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2024-10-20 19:01:29 -0300

    comentarios

--- main.py
+++ main.py
@@ -11,20 +11,27 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hola! Soy un bot creado con Telebot')
+    bot.reply_to(message, 'Bienvenido a PadelPals')
 
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, '/start /help /ver_fmt /exit /duplicar')
+    bot.reply_to(message, '/start /help /ver_fmt /exit /duplicar /registrarse')
 
 
+@bot.message_handler(commands=['registrarse'])
+def send_help(message):
+    bot.reply_to(message, 'asd')
+
+
+# esto es para pruebas borrarlo
 @bot.message_handler(commands=['ver_fmt'])
 def send_format(message):
     print(message)
     bot.reply_to(message, str(message))
 
 
+# esto es para pruebas borrarlo
 @bot.message_handler(commands=['duplicar'])
 def send_double(message):
     print(message)
@@ -44,13 +51,12 @@ def receive_exit(_message):
     return
 
 
-# Para todos
+# Para todos los mensajes (esto es para pruebas borrarlo)
 @bot.message_handler(func=lambda _message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
 
-# suponer que esta funcion envia mensajes fuera
 def duplicar(n):
     return n * 2
 
