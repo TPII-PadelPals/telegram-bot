46ac82e4f707f56d2a1f356f45ff6669907d5f96
TDPFIUBA <tdelpup@fi.uba.ar>
2024-11-12 02:32:57 -0300

    feat: add player location update handler

--- handlers/player/__init__.py
+++ handlers/player/__init__.py
@@ -1,9 +1,12 @@
 from .configurar_disponibilidad import handle_configure_availability
 from .configurar_zona import handle_configure_zone
+from .configurar_ubicacion import handle_configure_location
 
 PLAYER_MESSAGE_HANDLERS = [{"command": "configurar_disponibilidad",
                             "handler": handle_configure_availability},
                            {"command": "configurar_zona",
-                            "handler": handle_configure_zone}]
+                            "handler": handle_configure_zone},
+                            {"command": "configurar_ubicacion",
+                            "handler": handle_configure_location}]
 
 PLAYER_CALLBACK_HANDLERS = []
