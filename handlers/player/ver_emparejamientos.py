98369affbefbe70464bd8363ef569ed74ff5b732
dima1997 <franki.dimaria@gmail.com>
2024-11-13 17:47:51 -0300

    add: ver_emparejamientos con Markdown

--- handlers/player/ver_emparejamientos.py
+++ handlers/player/ver_emparejamientos.py
@@ -3,10 +3,13 @@ from telebot.types import Message
 
 from utils.get_from_env import get_from_env_lang, get_from_env_api
 
+COL_WIDTH = 20
+
+
 def handle_see_matches(message: Message, bot: TeleBot, get_api=get_from_env_api, get_len=get_from_env_lang):
     api_conection = get_api()
     language = get_len()
-    #text = message.text
+    # text = message.text
     # obtengo los matches
     id_telegram = message.from_user.username
     matches = api_conection.get_matches(id_telegram)
@@ -15,12 +18,31 @@ def handle_see_matches(message: Message, bot: TeleBot, get_api=get_from_env_api,
         text_response = language["MESSAGE_SEE_MATCHES_EMPTY"]
         bot.reply_to(message, text_response)
         return
-    text_response = language["MESSAGE_SEE_MATCHES"]
+
+    players = [language['PLAYER']]
+    courts = [language['COURT']]
+    times = [language['TIME']]
     for match in matches:
         other_player = match["player_id_1"]
         if other_player == id_telegram:
             other_player = match["player_id_2"]
-        text_response += (other_player + language["SEE_MATCHES_SEPARATOR"] + str(match["paddle_court_id"])
-                          + language["SEE_MATCHES_SEPARATOR"] + str(match["time_availability"])) + "\n"
+        players.append(str(other_player))
+        courts.append(str(match['paddle_court_id']))
+        times.append(str(language['TIME_NAMES']
+                     [str(match['time_availability'])]))
 
-    bot.reply_to(message, text_response)
+    players_col_width = max([len(player) for player in players])
+    courts_col_width = max([len(court) for court in courts])
+    times_col_width = max([len(time) for time in times])
+
+    text_response = "```\n"
+    text_response += language["MESSAGE_SEE_MATCHES"]
+    for player, court, time in zip(players, courts, times):
+        text_response += language['SEE_MATCHES_SEPARATOR'].join([
+            f"{player.ljust(players_col_width)}",
+            f"{court.ljust(courts_col_width)}",
+            f"{time.ljust(times_col_width)}",
+        ]) + "\n"
+    text_response += "```"
+
+    bot.reply_to(message, text_response, parse_mode="MarkdownV2")
