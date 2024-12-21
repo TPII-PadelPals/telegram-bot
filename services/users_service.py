9fbf5d4a70efea7b699d74ae80b62bc1e20b5921
dima1997 <franki.dimaria@gmail.com>
2024-12-21 03:39:19 -0300

    refactor: UsersService replace Config by settings

--- services/users_service.py
+++ services/users_service.py
@@ -1,18 +1,14 @@
 from .base_service import BaseService
-from model.config import Config
+from core.config import settings
 
 
 class UsersService(BaseService):
     def __init__(self):
         """Set the base URL for the service."""
         local_server = ["localhost", "127.0.0.1"]
-        host = f"{Config.USERS_SERVICE_HOST}:{Config.USERS_SERVICE_PORT}/api/v1"
-        self.base_url = f"http://{host}" if Config.USERS_SERVICE_HOST in local_server else f"https://{host}"
-        self.x_api_key_header = {"x-api-key": Config.USERS_SERVICE_API_KEY}
-
-    def register_user(self, chat_id):
-        """Register a user with the given chat ID."""
-        return self.post("/users", json={'chat_id': str(chat_id)})
+        host = f"{settings.USERS_SERVICE_HOST}:{settings.USERS_SERVICE_PORT}/api/v1"
+        self.base_url = f"http://{host}" if settings.USERS_SERVICE_HOST in local_server else f"https://{host}"
+        self.x_api_key_header = {"x-api-key": settings.USERS_SERVICE_API_KEY}
 
     def get_user_info(self, chat_id):
         """Get the information of a user with the given chat ID."""
