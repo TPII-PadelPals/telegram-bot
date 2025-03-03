8c7a41f538c0e245e62ff63e6deae63b6102a0ca
tdelpup <tomasgdelpup@gmail.com>
2025-03-03 19:40:50 -0300

    refactor: update location handling to use PlayersService and UsersService

--- handlers/player/configurar_ubicacion.py
+++ handlers/player/configurar_ubicacion.py
@@ -1,41 +1,47 @@
 from model.telegram_bot import TelegramBot
 from telebot.types import Message, ReplyKeyboardMarkup
-from services.player_service import PlayerServiceBackEnd
+from services.players_service import PlayersService
+from services.users_service import UsersService
+from uuid import UUID
 
-DEFAULT_PLAYER = 'francoMartinDiMaria'
+players_service = PlayersService()
+users_service = UsersService()
 
-player_service = PlayerServiceBackEnd()
-
-def handle_configure_location(message: Message, bot: TelegramBot):
+def handle_address_configuration(message: Message, bot: TelegramBot):
     chat_id = message.chat.id
+    users = users_service.get_user_info(chat_id)
+    user_public_id = users["data"][0]["public_id"]
 
     msg = bot.send_message(chat_id, "Por favor, ingrese la ubicación donde desea jugar:")
-    bot.register_next_step_handler(msg, process_location_step, bot)
+    bot.register_next_step_handler(msg, process_address_step, bot, user_public_id)
 
-def process_location_step(message: Message, bot: TelegramBot):
+def process_address_step(message: Message, bot: TelegramBot, user_public_id: UUID):
     chat_id = message.chat.id
-    location = message.text
-    nickname = message.from_user.username if message.from_user.username is not None else DEFAULT_PLAYER
+    address = message.text
+    player_partial_data = {
+        "address": address,
+    }
 
-    response = player_service.update_location(nickname, location)
+    response = players_service.update_partial_player(user_public_id, player_partial_data)
     if response:
         bot.send_message(chat_id, "¡Ubicación guardada con éxito! ¿Cuántos kilómetros está dispuesto a caminar hasta una cancha de pádel?")
         markup = ReplyKeyboardMarkup(one_time_keyboard=True)
         markup.add('<5', '<10', '<15', '<20', '<25', '<30')
         msg = bot.send_message(chat_id, "Elija un radio:", reply_markup=markup)
-        bot.register_next_step_handler(msg, process_radius_step, bot, location)
+        bot.register_next_step_handler(msg, process_radius_step, bot, user_public_id)
     else:
         bot.send_message(chat_id, "No se pudo guardar la ubicación. Por favor, inténtelo de nuevo.")
 
-def process_radius_step(message: Message, bot: TelegramBot, location: str):
+def process_radius_step(message: Message, bot: TelegramBot, user_public_id: str):
     chat_id = message.chat.id
     radius = message.text
-    nickname = message.from_user.username if message.from_user.username is not None else DEFAULT_PLAYER
-
     parsed_radius = int(radius.replace("<", ""))
+    player_partial_data = {
+        "search_range_km": parsed_radius,
+    }
 
-    response = player_service.update_radius(nickname, location, parsed_radius)
+    response = players_service.update_partial_player(user_public_id, player_partial_data)
     if response:
-        bot.send_message(chat_id, f"¡Radio de {radius} km guardado con éxito para la ubicación {location}!")
+        bot.send_message(chat_id, f"¡Radio de {radius} km guardado con éxito!")
     else:
         bot.send_message(chat_id, "No se pudo guardar el radio. Por favor, inténtelo de nuevo.")
