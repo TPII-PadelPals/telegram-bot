06b644ac52d462826e8fb8628eea9c7630c154bb
tdelpup <tomasgdelpup@gmail.com>
2025-03-05 00:32:45 -0300

    refactor: update callback string handling for availability configuration

--- handlers/player/configurar_disponibilidad.py
+++ handlers/player/configurar_disponibilidad.py
@@ -7,17 +7,21 @@ DEFAULT_PLAYER = 'francoMartinDiMaria'
 
 AVAILABILITY_CONFIGURATION_COMMAND = "configurar_disponibilidad"
 
+CALLBACK_STRING_SEPARATOR = ':'
+TIME = 'time'
+DAY = 'day'
+
 def filter_fn(call: CallbackQuery):
     return call.data.startswith(AVAILABILITY_CONFIGURATION_COMMAND)
 
 def generate_callback_string(data: str):
-    return f"{AVAILABILITY_CONFIGURATION_COMMAND}:{data}"
+    return f"{AVAILABILITY_CONFIGURATION_COMMAND}{CALLBACK_STRING_SEPARATOR}{data}"
 
 def availability_callback(call: CallbackQuery, bot: TelegramBot):
-    type = call.data.split(":")[1]
-    if type == 'time':
+    type = call.data.split(CALLBACK_STRING_SEPARATOR)[1]
+    if type == TIME:
         process_time_step(call, bot)
-    elif type == 'day':
+    elif type == DAY:
         process_day_step(call, bot)
     else:
         bot.send_message(
@@ -26,7 +30,7 @@ def availability_callback(call: CallbackQuery, bot: TelegramBot):
         )    
 
 def handle_configure_availability(message: Message, bot: TelegramBot):
-    buttons = [{'text': x["text"], 'callback_data': generate_callback_string(f"time:{x['callback_data']}")} for x in bot.language_manager.get("AVAILABILITY_TIME_BUTTONS")]
+    buttons = [{'text': x["text"], 'callback_data': generate_callback_string(f"{TIME}{CALLBACK_STRING_SEPARATOR}{x['callback_data']}")} for x in bot.language_manager.get("AVAILABILITY_TIME_BUTTONS")]
     menu = bot.ui.create_inline_keyboard(buttons, row_width=2)
     bot.send_message(message.chat.id, bot.language_manager.get("AVAILABLE_TIME_MESSAGE"), reply_markup=menu)
 
@@ -36,7 +40,7 @@ def process_time_step(call: CallbackQuery, bot: TelegramBot):
 
     callback_data: dict = call.data
     telegram_id = call.message.chat.username if call.message.chat.username else DEFAULT_PLAYER
-    time_id = int(callback_data.split(':')[-1])
+    time_id = int(callback_data.split(CALLBACK_STRING_SEPARATOR)[-1])
 
     if time_id is None:
         bot.reply_to(call.message, 'No se pudo configurar la disponibilidad')
@@ -45,7 +49,7 @@ def process_time_step(call: CallbackQuery, bot: TelegramBot):
     api_conection.set_availability(time_id, telegram_id)
     #bot.edit_message_text(call.message, 'Se ha configurado el horario correctamente')
 
-    buttons = [{'text': x["text"], 'callback_data': generate_callback_string(f"day:{x['callback_data']}")} for x in bot.language_manager.get("AVAILABILITY_DAY_BUTTONS")]
+    buttons = [{'text': x["text"], 'callback_data': generate_callback_string(f"{DAY}{CALLBACK_STRING_SEPARATOR}{x['callback_data']}")} for x in bot.language_manager.get("AVAILABILITY_DAY_BUTTONS")]
     menu = bot.ui.create_inline_keyboard(buttons, row_width=2)
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=bot.language_manager.get("AVAILABLE_DAYS_MESSAGE"), reply_markup=menu)
@@ -56,7 +60,7 @@ def process_day_step(call: CallbackQuery, bot: TelegramBot):
 
     callback_data: dict = call.data
     telegram_id = call.message.chat.username if call.message.chat.username else DEFAULT_PLAYER
-    day_id = int(callback_data.split(':')[-1])
+    day_id = int(callback_data.split(CALLBACK_STRING_SEPARATOR)[-1])
 
     if day_id is None:
         bot.reply_to(call.message, 'No se pudo configurar la disponibilidad')
