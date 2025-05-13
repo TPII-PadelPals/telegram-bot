881065c29327c6c544ca2cc54558d14b2ab6263b
dima1997 <franki.dimaria@gmail.com>
2025-05-13 15:33:38 -0300

    fix: ver_emparejamientos callback

--- handlers/player/matchups/handle_display_all_matchups.py
+++ handlers/player/matchups/handle_display_all_matchups.py
@@ -1,4 +1,4 @@
-from handlers.player.matchups.utils import generate_callback_string, parse_provisional_match, validate_and_filter_matchups
+from handlers.player.matchups.utils import MatchupAction, generate_callback_string, parse_provisional_match, validate_and_filter_matchups
 from model.telegram_bot import TelegramBot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
@@ -14,7 +14,8 @@ def matchups_keyboard_line(bot: TelegramBot, matchup: dict):
     button_text = f"{public_id} - {court_id} - {time} {date}"
     return InlineKeyboardButton(
         text=button_text,
-        callback_data=generate_callback_string(public_id)
+        callback_data=generate_callback_string(
+            f"{MatchupAction.ONE}:{public_id}")
     )
 
 
