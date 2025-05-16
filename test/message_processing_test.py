f3cc6b53df1686959455d932e200a9fa37692148
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-05-16 18:46:56 -0300

    SCRUM-446: translate new_match to ES (#30)
    When receives "new_match" notification constant send message in Spanish

--- test/message_processing_test.py
+++ test/message_processing_test.py
@@ -6,7 +6,10 @@ from utils.message_processing import MessageProcessing
 
 class TestMessageProcessing(unittest.TestCase):
     def setUp(self):
-        self.language = {"MESSAGE_SURVEY_PLAYER_ALLOWED": "MESSAGE_SURVEY_PLAYER_ALLOWED:"}
+        self.language = {
+            "MESSAGE_SURVEY_PLAYER_ALLOWED": "MESSAGE_SURVEY_PLAYER_ALLOWED:",
+            "MESSAGE_NEW_MATCHES": "MESSAGE_NEW_MATCHES"
+        }
         self.message_processing = MessageProcessing()
         self.request = MagicMock()
         self.request.chat_id = "123456"
@@ -23,6 +26,13 @@ class TestMessageProcessing(unittest.TestCase):
         self.request.message = message
         result = self.message_processing.message_processing(self.language, self.request)
         self.assertEqual(result["message"], "MESSAGE_SURVEY_PLAYER_ALLOWED:HOLA, MUNDO")
+        self.assertEqual(result["chat_id"], "123456")
+
+    def test_message_processing_new_matches(self):
+        message = "NEW_MATCH"
+        self.request.message = message
+        result = self.message_processing.message_processing(self.language, self.request)
+        self.assertEqual(result["message"], "MESSAGE_NEW_MATCHES")
         self.assertEqual(result["chat_id"], "123456")
 
 
