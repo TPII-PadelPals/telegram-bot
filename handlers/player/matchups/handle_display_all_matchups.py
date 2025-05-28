a7d8e3e2e200c669b16b20c99adc005397221bac
dima1997 <franki.dimaria@gmail.com>
2025-05-27 23:43:34 -0300

    refactor: remove markdown from MESSAGE_SEE_MATCHES

--- handlers/player/matchups/handle_display_all_matchups.py
+++ handlers/player/matchups/handle_display_all_matchups.py
@@ -52,15 +52,13 @@ def display_all_matchups(bot: TelegramBot, chat_id: int, message_id: int | None 
             chat_id=chat_id,
             message_id=message_id,
             text=bot.language_manager.get("MESSAGE_SEE_MATCHES"),
-            reply_markup=matchups_keyboard(bot, matches),
-            parse_mode='Markdown'
+            reply_markup=matchups_keyboard(bot, matches)
         )
     else:
         bot.send_message(
             chat_id=chat_id,
             text=bot.language_manager.get("MESSAGE_SEE_MATCHES"),
             reply_markup=matchups_keyboard(bot, matches),
-            parse_mode='Markdown'
         )
 
 
