f8c957e623c0001d47b70113aca54dc0c6a4e1bf
Franco Martin DI MARIA <franki.dimaria@gmail.com>
2025-07-13 16:12:32 -0300

    SCRUM-508: Fix payments flow to process payment before confirming match (#40)
        La operación de confirmar un partido se traslada al servicio de pagos
        Se refactoriza y divide la logica de confirmar y rechazar un partido

--- core/config.py
+++ core/config.py
@@ -22,24 +22,30 @@ class Settings(BaseSettings):
     GATEWAY_HOST: str
     GATEWAY_PORT: int
 
+    # Users service settings
+    USERS_SERVICE_HOST: str
+    USERS_SERVICE_PORT: int
+    USERS_SERVICE_API_KEY: str
+
     # Players service settings
     PLAYERS_SERVICE_HOST: str
     PLAYERS_SERVICE_PORT: int
     PLAYERS_SERVICE_API_KEY: str
 
-    # Users service settings
-    USERS_SERVICE_HOST: str
-    USERS_SERVICE_PORT: int
-    USERS_SERVICE_API_KEY: str
+    # Business service settings
+    BUSINESS_SERVICE_HOST: str
+    BUSINESS_SERVICE_PORT: int
+    BUSINESS_SERVICE_API_KEY: str
 
     # Matches service settings
     MATCHES_SERVICE_HOST: str
     MATCHES_SERVICE_PORT: int
     MATCHES_SERVICE_API_KEY: str
 
-    # Business service settings
-    BUSINESS_SERVICE_HOST: str
-    BUSINESS_SERVICE_PORT: int
-    BUSINESS_SERVICE_API_KEY: str
+    # Payments service settings
+    PAYMENTS_SERVICE_HOST: str
+    PAYMENTS_SERVICE_PORT: int
+    PAYMENTS_SERVICE_API_KEY: str
+
 
 settings = Settings()
